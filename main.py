c3ae230c664daf73fb893c1573e431d2936826d5
Denys Fedorov <6803074@stud.kai.edu.ua>
2026-04-06 01:56:21 +0300

    add log message on start

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ import logging)
 "def main(): print('Hello')" 
 "import logging" 
 "logging.basicConfig(level=logging.INFO)" 
+<<<<<<< HEAD
 >>>>>>> d290060 (setup logging config)
+=======
+"logging.info('Application started')" 
+>>>>>>> 5bd8cf5 (add log message on start)
